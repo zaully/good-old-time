a27814d6c2ff15a94042ecd59847cbf360856600
Vince Liang <elyrxi@gmail.com>
2017-04-15 01:25:54 -0400

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Vince Liang on 2017-04-15.
+//  Copyright © 2017 crawley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
