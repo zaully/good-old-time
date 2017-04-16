4d14602139b5682df052b3ef8b797a1220cbf04c
Vince Liang <elyrxi@gmail.com>
2017-04-16 15:39:38 -0400

    Solution 486
    using DP instead of binary search. reducing to O(n) space while speed still O(n2)

--- leetcode/solutions/4/8/Solution486.hpp
+++ leetcode/solutions/4/8/Solution486.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <stack>
 #include <vector>
 
 using std::vector;
@@ -8,102 +7,60 @@ using std::vector;
 class Solution486 {
   public:
     void run() {
-        vector<int> nums = {1, 5, 2, 1, 5, 2, 1, 5, 2, 1, 5, 2, 1, 5, 2, 1, 5, 2, 1, 5};
+        vector<int> nums = {1, 5, 2};
         PredictTheWinner(nums);
     }
 
     bool PredictTheWinner(vector<int> &nums) {
-        auto level = 0;
-        auto root = new TreeNode(0, 0, level++);
-        if (nums.size() > 0) {
-            addLayer(root, nums, level, 0, nums.size());
-        }
-        postorderDfsCalculateScores(root);
-        auto win = root->aScore >= root->bScore;
-        delete root;
-        return win;
-    }
-
-  private:
-    struct TreeNode {
-        int aScore;
-        int bScore;
-        int level;
-        TreeNode *left;
-        TreeNode *right;
-        TreeNode(int as, int bs, int lvl)
-                : aScore(as),
-                  bScore(bs),
-                  level(lvl),
-                  left(nullptr),
-                  right(nullptr) {}
-
-        ~TreeNode() {
-            if (left) {
-                delete left;
-                left = nullptr;
-            }
-            if (right) {
-                delete right;
-                right = nullptr;
-            }
-        }
-    };
-    
-    void postorderDfsCalculateScores(TreeNode* root) {
-        std::stack<TreeNode*> visitStack;
-        TreeNode *lastVisit = nullptr;
-        auto cur = root;
-        while (!visitStack.empty() || cur) {
-            if (cur) {
-                visitStack.push(cur);
-                cur = cur->left;
-            } else {
-                auto peek = visitStack.top();
-                if (peek->right && lastVisit != peek->right) {
-                    cur = peek->right;
+        /*
+         * use DP to calculate every sub-segment until the whole array
+         * 1. initialize an N-object array to store balance. balance will be the
+         * differences between player A and B
+         *
+         * 2. calculate 1 element, then 2, 3... until reaches the whole array
+         *
+         * 3. when calculate subSolution[i] in iteration #j
+         *    3a. player A will aim for higher balance while B aims for lower
+         *    3b. every subSolution[i] is calculated by last subSolution[i],
+         * subSolution[i + 1] and nums[i] and nums[i + nums.size() -
+         * calculateSize], where nums provide the new elements from both ends
+         *
+         * 4. when calculateSize reaches 0, subSolution[0] is the best possible
+         * balance for A.
+         */
+        
+        auto calculateSize = nums.size();
+        
+        // 1.
+        vector<int> subSolutions(calculateSize);
+        
+        while (calculateSize > 0) {
+        
+            // 3a.
+            auto pickHigher = calculateSize % 2 == 1;
+            
+            // 2.
+            for (auto i = 0; i < calculateSize; i++) {
+                if (subSolutions.size() == calculateSize) {
+                    subSolutions[i] = pickHigher ? nums[i] : -nums[i];
                 } else {
-                    auto leftAScore = peek->left ? peek->left->aScore : 0;
-                    auto rightAScore = peek->right ? peek->right->aScore : 0;
-                    auto leftBScore = peek->left ? peek->left->bScore : 0;
-                    auto rightBScore = peek->right ? peek->right->bScore : 0;
-                    if (peek->level % 2 == 0) {
-                        peek->aScore += leftAScore > rightAScore ? leftAScore : rightAScore;
-                        peek->bScore += leftBScore < rightBScore ? leftBScore : rightBScore;
-                    } else {
-                        peek->aScore += leftAScore < rightAScore ? leftAScore : rightAScore;
-                        peek->bScore += leftBScore > rightBScore ? leftBScore : rightBScore;
-                    }
-                    
-                    lastVisit = peek;
-                    visitStack.pop();
+                    // 3b.
+                    auto score1 = pickHigher ? nums[i] : -nums[i];
+                    auto score2 = nums[i + nums.size() - calculateSize];
+                    score2 = pickHigher ? score2 : -score2;
+                    score1 += subSolutions[i + 1];
+                    score2 += subSolutions[i];
+                    subSolutions[i] =
+                            (pickHigher ? score1 < score2 : score1 > score2)
+                                    ? score2
+                                    : score1;
                 }
             }
+            // 2.
+            calculateSize--;
         }
+        
+        // 4.
+        return subSolutions.size() <= 0 || subSolutions[0] >= 0;
     }
-
-    void addLayer(TreeNode *current, const vector<int> &nums, int level,
-                  size_t leftBoundary, size_t rightBoundary) {
-        auto newNode = [&nums, &level](size_t index){
-            int aScore, bScore;
-            if (level % 2 == 1) {
-                aScore = nums[index];
-                bScore = 0;
-            } else {
-                bScore = nums[index];
-                aScore = 0;
-            }
-            return new TreeNode(aScore, bScore, level);
-        };
-        if (leftBoundary < rightBoundary) {
-            current->left = newNode(leftBoundary);
-            addLayer(current->left, nums, level + 1, leftBoundary + 1,
-                     rightBoundary);
-        }
-        if (leftBoundary < rightBoundary - 1) {
-            current->right = newNode(rightBoundary - 1);
-            addLayer(current->right, nums, level + 1, leftBoundary,
-                     rightBoundary - 1);
-        }
-    };
 };
